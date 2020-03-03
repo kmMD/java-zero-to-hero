4d27e58173aaf7cdfa991f834a3949f7bf5601f5
Kristjan-Martin Kirjanen <krkirj@ttu.ee>
2020-03-03 08:41:40 +0200

    level 0 solution

--- src/main/java/ee/itcollage/level0/HelloWorld.java
+++ src/main/java/ee/itcollage/level0/HelloWorld.java
@@ -3,6 +3,8 @@ package ee.itcollage.level0;
 public class HelloWorld {
 
     public static void main(String[] args) {
-        //todo print out hello world and then your name on the next line
+        // print out hello world and then your name on the next line
+        System.out.println("Hello world!");
+        System.out.println("Kristjan-Martin Kirjanen");
     }
 }
--- src/main/java/ee/itcollage/level0/NumberWorld.java
+++ src/main/java/ee/itcollage/level0/NumberWorld.java
@@ -3,6 +3,7 @@ package ee.itcollage.level0;
 public class NumberWorld {
 
     public static void main(String[] args) {
-        //todo print out 5 + 3 = equals and then print out the sum
+        // print out 5 + 3 = equals and then print out the sum
+        System.out.println("5 + 3 = equals " + (5 + 3));
     }
 }
